da441bc084d3179a31378fb30191215bf0e81c0b
Tomas Halman <tomas@halman.net>
2021-03-21 16:31:19 +0100

    Import openmusic theme

---
+++ openmusic_barrio/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+'use strict';
+
+
+
+(function ($, Drupal) {
+  Drupal.behaviors.openmusic_barrio = {
+    attach: function (context, settings) {
+
+    }
+  };
+})(jQuery, Drupal);
